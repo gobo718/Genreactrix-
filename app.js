cbbc3d5b3ccda9a6660e45f8b0ef3fc15c0fcfd8
gobo718 <gobo718@gmail.com>
2026-08-04 22:12:30 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const GENREACTRIX_BUILD="v0.9.13.0";
+const GENREACTRIX_BUILD="v0.9.14.0";
 const PRIMFUSION_LABEL_FIT = Object.freeze({ preferredPx: 9, stepPx: 0.25, allowedShrinkRatio: 0.15, individualMinimumPx: 1 });
 function setDirectorStatus(message){
   const status=$("directorStatus");
